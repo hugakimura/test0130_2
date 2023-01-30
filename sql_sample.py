fb832c0d145641bd5bc4c8da9973d41d8b3248a6
木村 鳳雅 <huga.kimura@intra.udom.co.jp>
2023-01-30 18:39:24 +0900

    pushコマンド1

--- sql_sample.py
+++ sql_sample.py
@@ -7,10 +7,15 @@ import pandas as pd
 
 import config
 
-user = config.DB_USER
-password = config.PASSWORD
-host = config.HOST
-db_name = config.DATABASE
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+user = config.os.getenv('DB_USER')
+password = config.os.getenv('PASSWORD')
+host = config.os.getenv('HOST')
+db_name = config.os.getenv('DATABASE')
 
 # engineの設定
 engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{db_name}')
@@ -43,30 +48,32 @@ class Wine(Base):
   wine_class = Column(Integer, unique=False)
   alcohol = Column(Float, unique=False)
   ash = Column(Float, unique=False)
-  magnesium = Column(Float, unique=False)
   hue = Column(Float, unique=False)
   proline = Column(Integer, unique=False)
   
-  def __init__(self, wine_class=None, alcohol=None, ash=None,magnesium =None, hue=None, proline=None):
+  def __init__(self, wine_class=None, alcohol=None, ash=None, hue=None, proline=None):
     self.wine_class = wine_class
     self.alcohol = alcohol
     self.ash = ash
-    self.magnesium =magnesium
     self.hue = hue
     self.proline = proline
 
 Base.metadata.create_all(bind=engine)
-
 def read_data():
   """CSVファイルを読み込み、DBにデータを追加する関数
   """
-  wine_df = pd.read_csv('./data/wine_class.csv')
+  wine_df = pd.read_csv('./wine_class.csv')
 
   for index, _df in wine_df.iterrows():
-    row = Wine(wine_class=_df['Class'], alcohol=_df['Alcohol'], ash=_df['Ash'], magnesium=_df['Magnesium'], hue=_df['Hue'], proline=_df['Proline'])
+    row = Wine(wine_class=_df['Class'], alcohol=_df['Alcohol'], ash=_df['Ash'], hue=_df['Hue'], proline=_df['Proline'])
     # データを追加する
     db_session.add(row)
 
   db_session.commit()
 
 read_data()
+
+db = db_session.query(Wine).all()
+for row in db:
+  # カラムを指定してデータを取得する
+  print(row.alcohol)
